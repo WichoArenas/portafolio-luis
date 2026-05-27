e313ae71209d19116fe820007e61bd233601286a
Luis Arenas <wicholuiz29@gmail.com>
2026-05-27 13:02:35 -0700

    Primera version de mi portafolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // SUSTITUYE 'WichoArenas' por tu usuario exacto de GitHub
+  site: 'https://WichoArenas.github.io', 
+  
+  // SUSTITUYE 'portafolio-luis' por el nombre EXACTO de tu repositorio
+  base: '/portafolio-luis', 
+  
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
